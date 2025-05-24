14fe97143de1579a022561b74f17c99a1345d501
Yu Fang <newfarry@126.com>
2025-05-24 18:41:58 +0800

    update

---
+++ libs/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/alist-org/alist/v3/libs/mobile"
+
+func main() {
+	mobile.Run()
+}
--- libs/mobile/lib.go
+++ libs/mobile/lib.go
@@ -5,8 +5,41 @@ import (
 	"sync"
 )
 
+var callback = new(logCallBack)
+
+type logCallBack struct {
+}
+
+func (l *logCallBack) OnLog(level int16, time int64, message string) {
+	log.Println(level, time, message)
+}
+
+var eventEntity = new(EventEntity)
+
+type EventEntity struct {
+}
+
+func (e EventEntity) OnStartError(t string, err string) {
+	//TODO implement me
+	log.Println(t, err)
+}
+
+func (e EventEntity) OnShutdown(t string) {
+	log.Println(t)
+}
+
+func (e EventEntity) OnProcessExit(code int) {
+	log.Println(code)
+}
+
 func Run() {
 	log.Println("Start service")
+	SetConfigData("/data/user/0/cloud.iothub.apps.alistweb.alist_web/app_flutter")
+	err := Init(eventEntity, callback)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	Start()
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	wg.Wait()
